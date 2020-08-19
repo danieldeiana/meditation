2bc069c4751bbc4575ccf45c22c12ee56040f5c7
Daniel Deiana <danieldeiana84@gmail.com>
2020-08-19 15:29:20 +0100

    lots of work and styling, should have commited earlier!

--- meditationascensionapp/views.py
+++ meditationascensionapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import MailListForm
 from .models import Testimonial
 
 
@@ -28,4 +29,18 @@ def blog(request):
 
 
 def contact(request):
-    return render(request, 'meditationascensionapp/contact.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = MailListForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = MailListForm()
+    return render(request, 'meditationascensionapp/contact.html', {'form': form})
